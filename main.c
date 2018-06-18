12848947f29ca1cf9e5a541748d38561cfdfa6b4
Ximsa <burrfelix@aol.de>
2018-06-18 22:43:24 +0200

    plotted a function!

--- main.c
+++ main.c
@@ -7,30 +7,27 @@
 
 int main()
 {
-	Polygon* pol = 0;
-	Polygon* pol2 = 0;
-
-	pol = Polygon_create(pol);
-	pol2 = Polygon_create(pol2);
-
-	Polygon_add(pol, 150 , 100);
-	Polygon_add(pol, 150 , 50);
-	Polygon_add(pol, 200 , 100);
-
-	Polygon_add(pol2, 1000 , 1000);
-	Polygon_add(pol2, 1500 , 500);
-	Polygon_add(pol2, 2000 , -1000);
-	
-	Terrain* ter = 0;
+	Terrain *ter = 0;
 	ter = Terrain_create(ter);
+	float ly = 0;
+	for(float x = -999; x < 1000; x++)
+	{
 
-	Terrain_add(ter, pol, "environment/alpine/ground/ground1.dds", 1, true, true, true, true, true);
-	Terrain_add(ter, pol2, "environment/alpine/ground/ground1.dds", 1, true, true, true, true, true);
+		float y = (sin(0.006*x*cos(0.005*x+cos(0.004*x))) + sin(0.05*x)) * 5;
+		Polygon *pol = 0;
+		pol = Polygon_create(pol);
+		Polygon_add(pol, 0 , 100000);
+		Polygon_add(pol, (x-1)*10, ly * 1000);
+		Polygon_add(pol, x*10, y*1000);
+
+		Terrain_add(ter, pol, "environment/alpine/ground/ground1.dds", 1, true, true, true, true, false);
+		ly = y;
+	}
 
 	Memory* header = 0;
 	header = FweHeader_create(header, "environment/alpine");
 	Memory* mem = Terrain_toMemory(ter);
-	Util_writeVoidArrayFile(0,"result.fwe", header->data, header->size);
-	Util_writeVoidArrayFile(1,"result.fwe", mem->data, mem->size);
+	Util_writeVoidArrayFile(0,"test_2.fwe", header->data, header->size);
+	Util_writeVoidArrayFile(1,"test_2.fwe", mem->data, mem->size);
 	return 0;
 }
